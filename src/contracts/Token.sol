18882ed8c201d73d261aa9014f79f672f757be9e
Benito González <benitogonzalezh@gmail.com>
2021-11-15 11:53:02 -0300

    update Token Contract with standart methods

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,6 +1,35 @@
 pragma solidity ^0.5.0;
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-contract Token{
-  string public name = "Token";
+contract Token {
+    using SafeMath for uint256;
 
+    //Variables
+    string public name = "DApp Token";
+    string public symbol = "DAPP";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    //Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    //Track Balances
+    mapping(address => uint256) public balanceOf;
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(_to != address(0));
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    constructor() public {
+        totalSupply = 1000000 * (10**decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
 }
